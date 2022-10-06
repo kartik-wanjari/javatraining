d234a11e3832fdf0608cebc735ab4d083d45fd70
Kartik Wanjari <wanjarik70@gmail.com>
2022-10-06 22:47:27 +0530

    create barchart from restapi

--- charts/BarGraph.java
+++ charts/BarGraph.java
@@ -15,8 +15,16 @@ import org.apache.logging.log4j.Logger;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.ItemLabelAnchor;
+import org.jfree.chart.labels.ItemLabelPosition;
+import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.CategoryItemRenderer;
+import org.jfree.chart.ui.TextAnchor;
 import org.jfree.data.category.DefaultCategoryDataset;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.File;
@@ -25,11 +33,10 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
+
 
 /**
- * This class generate bar graph of temperature of different cities.
+ * This class generate bar graph of price vs product of fake store API.
  */
 public class BarGraph {
     public static Logger logger = LogManager.getLogger(BarGraph.class);
@@ -63,56 +70,52 @@ public class BarGraph {
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        logger.info("Starting to fetch response from weather API using GET request");
-        String[] places = {"Delhi", "London", "Dubai", "Paris"};
-        List<String> allResponse = new ArrayList<>();
-        for (String i : places) {
-            logger.debug("Fetching weather data of " + i);
-            logger.info("Fetching data from weather api");
-            logger.warn("Put valid weather api url");
-            HttpResponse<String> res = response("http://api.weatherapi.com/v1/current.json?key=18858d6d3d5e43bb8db40153222009&q=" + i + "&aqi=no");
+        logger.info("Starting to fetch response from Fake store API using GET request");
 
-            if (res.statusCode() == 200) {
-                allResponse.add(res.body());
-                logger.debug("Object Data: " + res.body());
-            } else {
-                logger.error("Status code: " + res.statusCode());
-            }
+        HttpResponse<String> res = response("https://fakestoreapi.com/products");
+        JSONArray jsonArray = null;
+        if (res.statusCode() == 200) {
+            jsonArray = new JSONArray(res.body());
+            logger.debug("Object Data: " + res.body());
+        } else {
+            logger.error("Status code: " + res.statusCode() + "response not fetched");
         }
-        List<String> allTemp = new ArrayList<>();
-        for (String i : allResponse) {
 
-            JSONObject jsonObject = new JSONObject(i);
-            logger.info("Fetching value of key(current) from JSONObject");
-            JSONObject current = jsonObject.getJSONObject("current");
-            logger.debug("Current Object: " + current.toString());
-            allTemp.add(current.toString());
-        }
-        int m = 0;
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        for (String i : allTemp) {
-            JSONObject temp = new JSONObject(i);
-            dataset.addValue(Double.valueOf(temp.get("temp_c").toString()), "Celsius", places[m]);
-            logger.debug("Celsius temperature: " + Double.valueOf(temp.get("temp_c").toString()));
-            dataset.addValue(Double.valueOf(temp.get("temp_f").toString()), "Fahrenheit", places[m]);
-            logger.debug("Fahrenheit temperature: " + Double.valueOf(temp.get("temp_f").toString()));
-            m++;
-        }
+        if(jsonArray!=null) {
+            for (Object i : jsonArray) {
+
+                JSONObject jsonObject = new JSONObject(i.toString());
+                System.out.println(jsonObject);
+                System.out.println(jsonObject.get("price").toString() + " " + jsonObject.get("title").toString());
+                logger.debug("title: " + jsonObject);
+                logger.debug("Price: " + jsonObject);
+                dataset.addValue(Double.valueOf(jsonObject.get("price").toString()), "Products", jsonObject.get("title").toString());
+            }
+        }else logger.error("JsonArray is null");
         logger.info("Creating bar chart of temperature vs city");
         JFreeChart barChart = ChartFactory.createBarChart(
-                "Temperature Bar Graph",
-                "City",
-                "Temperature",
+                "Product Price Comparison",
+                "Products",
+                "Price(USD)",
                 dataset,
-                PlotOrientation.VERTICAL,
-                true, true, false);
+                PlotOrientation.HORIZONTAL,
+                false, true, false);
+        logger.info("Rendering price on bar");
+        CategoryItemRenderer renderer = ((CategoryPlot)barChart.getPlot()).getRenderer();
+
+        renderer.setDefaultItemLabelGenerator(new StandardCategoryItemLabelGenerator());
+        renderer.setDefaultItemLabelsVisible(true);
+        ItemLabelPosition position = new ItemLabelPosition(ItemLabelAnchor.OUTSIDE12,
+                TextAnchor.TOP_RIGHT);
+        renderer.setDefaultPositiveItemLabelPosition(position);
         try {
             logger.info("Creating image Bar Graph");
-            ChartUtils.saveChartAsJPEG(new File("/Users/azuga/Documents/Azuga Training/Charts/Bar Graph.jpeg"), barChart, 500, 300);
-            System.out.println("Bar Graph.JPEG created");
+            ChartUtils.saveChartAsJPEG(new File("/Users/azuga/Documents/Azuga Training/Charts/bargraph.jpeg"), barChart, 900, 700);
+            System.out.println("bargraph.jpeg created");
             logger.info("End of program");
-        } catch (Exception e) {
-            System.err.println("error");
+        } catch (IOException e) {
+            System.err.println("IOException:" + e);
             logger.error("Destination path of file not valid");
         }
     }
